78e43a5c7eb9fb823c7921cecf69491ec23b30d1
Jackie Yang <jackie19970410@gmail.com>
2023-12-19 21:18:36 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jackie Yang on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
